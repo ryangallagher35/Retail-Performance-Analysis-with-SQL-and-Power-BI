2971011408a66a2cf42b33844cb54c20810109b6
Ryan Gallagher <148814347+ryangallagher35@users.noreply.github.com>
2025-05-12 21:15:58 -0400

    Update inventory_optimization.sql

--- SQL Files/inventory_optimization.sql
+++ SQL Files/inventory_optimization.sql
@@ -33,3 +33,15 @@ FROM Superstore
 GROUP BY Product_Name
 ORDER BY COUNT(DISTINCT ORDER_ID) DESC
 LIMIT 20; 
+
+-- The query below identifies items that are sometimes purchased together, illuminating opportunities for bundling. 
+SELECT 
+    a.Product_Name AS Product_1,
+    b.Product_Name AS Product_2,
+    COUNT(*) AS Times_Bundled
+FROM Superstore a
+JOIN Superstore b 
+    ON a.Order_ID = b.Order_ID 
+    AND a.Product_Name < b.Product_Name
+GROUP BY a.Product_Name, b.Product_Name
+ORDER BY Times_Bundled DESC;
